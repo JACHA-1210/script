28b7c98ecde360a470042114036311dfd8ff7471
Jose Andres Chero Ascurra <115717398+JACHA-1210@users.noreply.github.com>
2024-01-31 16:00:34 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import subprocess
 import importlib
 import bot  # Importa directamente el módulo bot
 
-def run_bot():
+def run():
     # Define la dirección IP y el puerto del servidor
     host = "172.16.22.190"
     port = 5555
